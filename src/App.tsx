51ccf80c5e00c3940f2f709968c7149384a5290f
Amritaash Verma <amritaash.verma@gmail.com>
2024-08-26 13:04:33 +0530

    the first ever change from terminal

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ return (
               <li>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
               </li>
+	      <li> <Darkthemetoggle /> </li>
             </ul>
           </div>
           <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
