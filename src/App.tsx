bc323a79cf7b8096708e93f26fba4cea2e5ba8d2
Amritaash Verma <amritaash.verma@gmail.com>
2024-08-26 13:08:35 +0530

    mods

--- src/App.tsx
+++ src/App.tsx
@@ -35,8 +35,7 @@ return (
               <li>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
               </li>
-	      <li> <Darkthemetoggle /> </li>
-            </ul>
+	   </ul>
           </div>
           <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
@@ -62,6 +61,7 @@ return (
           <li>
             <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
           </li>
+	  <li><DarkThemeToggle /></li>
         </ul>
       </div>
       </div>
